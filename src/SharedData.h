ab6eea6746013221aa1314a41dd36e15cc6fcac3
Marcel Schwittlick <lzrm@mrzl.net>
2014-05-04 14:19:55 +0200

    adding some global var to the shareddata

--- src/SharedData.h
+++ src/SharedData.h
@@ -5,11 +5,15 @@
 class SharedData
 {
 public:
-	std::string ANIMATION_STATE, FOLLOW_BALL_STATE, RECREATE_STATE;
+	std::string FOLLOW_BALL_STATE, DRAWING_STATE, RECREATE_STATE;
 	std::string MOUSE_MOVEMENT_FILENAME, POINTS_FILENAME;
 	wng::ofxCsv mouseMovementCsvRecorder, pointsCsvExporter;
 
 	ofVec2f currentMousePos;
 	std::vector< ofVec2f > points;
 	ofTrueTypeFont font;
+
+	std::string COMPUTER_COMPUTER, COMPUTER_HUMAN, HUMAN_COMPUTER, HUMAN_HUMAN;
+
+	std::string stateSelectionRadioButton;
 };
